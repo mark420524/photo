3ec0773a07fb061a23401c267a455892c5156742
mark <markjava@foxmail.com>
2022-04-29 21:38:26 +0800

    切换背景好了呀

--- pages/preview/index.js
+++ pages/preview/index.js
@@ -3,13 +3,26 @@ Page({
         item:{},
         currentImage:'/static/images/background.png',
         background:'white',
-        
+        active:0,
         currentHeight:0,
         currentWidth:0,
         calcWidth:0,
         calcHeight:0,
         showScale: 1,
-        unit:'px'
+        unit:'px',
+        colors:[
+            {name:'white',color:'white'},
+            {name:'red',color:'red'},
+            {name:'blue1',color:'#438edb'},
+            {name:'blue2',color:'#00bff3'},
+        ],
+        color:'',
+        customeStyle:[
+            'display:none;',
+            'display:none;',
+            'display:none;',
+            'display:none;'
+        ]
     },
     onLoad(){
         let width = 295;
@@ -30,4 +43,23 @@ Page({
             calcHeight:height
         })
     },
+    selectType(e){
+        let detail = e.detail;
+        console.log(detail)
+    },
+    changeBackground(e){
+        let dataset = e.currentTarget.dataset;
+        let color = dataset.color;
+        let index = dataset.index;
+        let customeStyle=[];
+        for (let i=0;i<this.data.colors.length;i++){
+            customeStyle[i]='display:none;';
+        }
+        customeStyle[index]='';
+        this.setData({
+            customeStyle:customeStyle,
+            color:color,
+            background:color
+        })
+    }
 })
